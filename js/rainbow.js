3227b68681b9bfa177f16d7cedf16263850b7336
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-04-20 13:25:56 +0200

    Add files via upload

---
+++ js/rainbow.js
@@ -0,0 +1,28 @@
+
+function myfunction1 () {
+    document.getElementById("red").style.background  = 'red'
+}
+
+function myfunction2 () {
+    document.getElementById("orange").style.background  = 'orange'
+}
+
+function myfunction3 () {
+    document.getElementById("yellow").style.background  = 'yellow'
+}
+
+function myfunction4 () {
+    document.getElementById("green").style.background  = 'green'
+}
+
+function myfunction5 () {
+    document.getElementById("blue").style.background  = 'blue'
+}
+
+function myfunction6 () {
+    document.getElementById("purple").style.background  = 'purple'
+}
+
+function myfunction7 () {
+    document.getElementById("violet").style.background  = 'violet'
+}
